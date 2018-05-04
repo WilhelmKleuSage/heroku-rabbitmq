6083c39b4a63709d111cd1e87ead2344dc2061d0
Wilhelm Kleu <wilhelm.kleu@sage.com>
2018-05-04 11:16:36 +0200

    Download file and read content as part of release phase

--- ReleasePhase/Program.cs
+++ ReleasePhase/Program.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
+using System.Net;
 using TestLibrary;
 
 namespace ReleasePhase
 {
     class Program
     {
+        private const string DownloadLinkKey = "DOWNLOAD_LINK";
+
+        private static void TestRetrievalOfFile(string url)
+        {
+            using (var wc = new WebClient())
+            {
+                Console.WriteLine($"Downloading from {url}");
+                wc.DownloadFile(new Uri(url), "current.txt");
+                var content = File.ReadAllText("current.txt");
+                Console.WriteLine($"File Content: {content}");
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Running release phase");
 
             Console.WriteLine("Environment variables:");
-            foreach (DictionaryEntry env in Environment.GetEnvironmentVariables())
+            var variables = Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry env in variables)
             {
                 Console.WriteLine("{0}={1}", env.Key, env.Value);
             }
 
             TestClass.PrintSomething("Test 123");
+            TestRetrievalOfFile(variables[DownloadLinkKey].ToString());
         }
     }
 }
